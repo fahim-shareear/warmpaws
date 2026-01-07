f629b092b455053febc5b0d89713938960b5a9d3
Fahim <fahimshareear@gmail.com>
2026-01-07 13:37:18 +0600

    setting up the authprovider evvironment

--- src/Components/Authentications/AuthProvider.jsx
+++ src/Components/Authentications/AuthProvider.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import { auth } from '../Firebase/firebase.init';
+
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
 
+    const googlePopUp = () =>{
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider);
+    }
+
+
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInUser = (email, password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const signOutUser = () =>{
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            setUser(currentUser);
+            setLoading(false);
+
+        });
+        return () =>{
+            unsubscribe();
+        };
+    }, []);
 
     const authInfo = {
+        user, createUser, signInUser, signOutUser, googlePopUp, loading
 
     }
     return (
